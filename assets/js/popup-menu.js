8afe97b265324bdfa294a3d31cf3813efbdd4104
minetaro12 <osawa2003@gmail.com>
2022-12-18 22:44:10 +0900

    メニュー部分をポップアップに変更

---
+++ assets/js/popup-menu.js
@@ -0,0 +1,6 @@
+const togglepopup = document.querySelector("#menu-button")
+const popup = document.querySelector(".popup-menu")
+
+togglepopup.addEventListener("click", () => {
+  popup.classList.toggle("hide")
+})
