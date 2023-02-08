7918a535bde2ee3c3adbf589eaaa8bc23d7f0030
minetaro12 <osawa2003@gmail.com>
2023-02-08 22:31:21 +0900

    ポップアップメニューの外側クリックで閉じるように

--- assets/js/popup-menu.js
+++ assets/js/popup-menu.js
@@ -1,6 +1,13 @@
 const togglepopup = document.querySelector("#menu-button")
 const popup = document.querySelector("#popup-menu")
+const main = document.querySelector("#main")
 
 togglepopup.addEventListener("click", () => {
   popup.classList.toggle("hide")
+})
+
+main.addEventListener("click", (event) => {
+  if(event.target.closest("#popup-menu") === null && event.target.closest("#menu-button") === null) {
+    popup.classList.add("hide")
+  }
 })
