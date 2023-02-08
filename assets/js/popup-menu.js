ebe5b73af29fd5ad47f08a02ff69bcfe507e0534
minetaro12 <osawa2003@gmail.com>
2023-02-08 21:52:41 +0900

    ポップアップ部分をidに

--- assets/js/popup-menu.js
+++ assets/js/popup-menu.js
@@ -1,5 +1,5 @@
 const togglepopup = document.querySelector("#menu-button")
-const popup = document.querySelector(".popup-menu")
+const popup = document.querySelector("#popup-menu")
 
 togglepopup.addEventListener("click", () => {
   popup.classList.toggle("hide")
