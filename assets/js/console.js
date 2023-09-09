a3180e899391704116e482a57651ef08f8a2d7e5
minetaro12 <osawa2003@gmail.com>
2023-09-09 20:30:32 +0900

    コンソールにメッセージが表示されるように

---
+++ assets/js/console.js
@@ -0,0 +1,6 @@
+(function() {
+  "use strict";
+
+  console.log("%cHello there!👋", "font-size: 2rem; font-weight: bold;");
+  console.log("If you're interested in the source code, you can find it here\nhttps://github.com/minetaro12/hugo-osan-theme");
+}())
