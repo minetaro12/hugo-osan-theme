008b1e1b3f3cffd038ec08ad0f1cf031305200d5
minetaro12 <osawa2003@gmail.com>
2023-11-03 10:47:07 +0900

    画像のポップアップが動作しないのを修正

--- assets/js/single/lib/imgPopup.js
+++ assets/js/single/lib/imgPopup.js
@@ -1,5 +1,5 @@
 export default () => {
-  document.querySelectorAll(".post-content .img-link").forEach((element) => {
+  document.querySelectorAll("#post-content .img-link").forEach((element) => {
     element.style.cursor = "zoom-in"
     element.addEventListener("click", (event) => {
       // リンク遷移をキャンセル
