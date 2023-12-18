1e16da641916ab635561c08dfe8daf5581ce7b19
minetaro12 <osawa2003@gmail.com>
2023-12-18 22:36:08 +0900

    ポップアップメニューのスタイルを変更

--- assets/js/base/lib/popupMenu.js
+++ assets/js/base/lib/popupMenu.js
@@ -1,14 +1,13 @@
 export default () => {
   const menuButton = document.querySelector("#menu-button")
   const popupMenu = document.querySelector("#popup-menu")
-  const main = document.querySelector("#main")
 
   menuButton.addEventListener("click", () => {
     popupMenu.classList.toggle("hide")
   })
 
-  main.addEventListener("click", (event) => {
-    if (event.target.closest("#popup-menu") === null && event.target.closest("#menu-button") === null) {
+  popupMenu.addEventListener("click", (event) => {
+    if (event.target.closest("#popup-menu-content") == null) {
       popupMenu.classList.add("hide")
     }
   })
