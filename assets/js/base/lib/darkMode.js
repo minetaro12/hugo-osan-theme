ee678f7975516f75ed02b989a59a09668665197a
minetaro12 <77948036+minetaro12@users.noreply.github.com>
2025-02-25 20:57:04 +0900

    refactor

--- assets/js/base/lib/darkMode.js
+++ assets/js/base/lib/darkMode.js
@@ -1,20 +1,20 @@
 export default () => {
   const switchMode = (changeDark) => {
     if (changeDark) {
-      document.documentElement.classList.add("Dark")
-      localStorage.setItem("dark", "on")
-      return
+      document.documentElement.classList.add("Dark");
+      localStorage.setItem("dark", "on");
+    } else {
+      document.documentElement.classList.remove("Dark");
+      localStorage.setItem("dark", "off");
     }
-    document.documentElement.classList.remove("Dark")
-    localStorage.setItem("dark", "off")
-  }
+  };
 
-  const lightButton = document.querySelector("#theme-button-light")
+  const lightButton = document.querySelector("#theme-button-light");
   lightButton.addEventListener("click", () => {
-    switchMode(false)
-  })
-  const darkButton = document.querySelector("#theme-button-dark")
+    switchMode(false);
+  });
+  const darkButton = document.querySelector("#theme-button-dark");
   darkButton.addEventListener("click", () => {
-    switchMode(true)
-  })
-}
+    switchMode(true);
+  });
+};
